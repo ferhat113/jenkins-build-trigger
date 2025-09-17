76981fc46a4979a2d6a8d0258229da2d059f200d
ferhat113 <88292243+ferhat113@users.noreply.github.com>
2025-09-17 11:07:23 +0100

    Update script.py
    adding date

--- script.py
+++ script.py
@@ -2,3 +2,14 @@ print('This is the final text')
 print('########################')
 print('adding new stuff..')
 print('########################')
+
+from datetime import datetime
+
+# Get the current date and time
+now = datetime.now()
+
+# Format the date and time
+current_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# Print the current date and time
+print("Current Date and Time:", current_date_time)
